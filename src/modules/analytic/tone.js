8fdae61913e114a3cd8742019f954f68b18c9bcb
Yudi <abd.rahmat.wahyudi@gmail.com>
2023-12-11 18:46:09 +0700

    ADD COLUMN NEWS

--- src/modules/analytic/tone.js
+++ src/modules/analytic/tone.js
@@ -14,6 +14,14 @@ const ToneTabs = ({
 }) => {
 	return (
 		<Row gutter={[gutter, gutter]}>
+			<Col xs={24} md={24} lg={12} xl={12}>
+				<Card title={coverage.title} onLoading={onLoading} {...cards}>
+				</Card>
+			</Col>
+			<Col xs={24} md={24} lg={12} xl={12}>
+				<Card title={coverage.title} onLoading={onLoading} {...cards}>
+				</Card>
+			</Col>
 			<Col xs={24} md={24} lg={full ? 12 : 24} xl={full ? 24 : 12}>
 				<Card title={coverage.title} onLoading={onLoading} {...cards}>
 					<ApexChart className={className} id='coverage' type='bar' {...coverage} />
