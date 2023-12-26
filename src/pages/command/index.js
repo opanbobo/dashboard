e66f9b9fda59f9c6469c4e1ded63be3777a68896
Abdul Rahmat Wahyudi <abd.rahmat.wahyudi@gmail.com>
2023-12-26 23:36:01 +0700

    Finally works

--- src/pages/command/index.js
+++ src/pages/command/index.js
@@ -310,7 +310,7 @@ dispatch(
       maxSize: body.maxSize,
       page: body.page,
       media_id: toneMedia.result.data[body.data.indexing].media_id,
-      tone: `${body.data.tone - 1}`,
+      tone: `${body.data.tone}`,
     })
   );
 
